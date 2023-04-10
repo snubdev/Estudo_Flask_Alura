a62f0a484ca3980fdfd4a3b427fef25b30f85909
Maicon Leandro <snubhousedev@gmail.com>
2023-04-10 16:13:46 -0300

    Listando jogos com flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
 
+
+class Jogo():
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 # Iniciar o variavel do projeto
 app = Flask(__name__)
 
 # Criar um nova rota
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Ataria')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 # Rodar o projeto
