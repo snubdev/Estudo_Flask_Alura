aabc2139741409517e1d12099cc09bf658b951be
Maicon Leandro <snubhousedev@gmail.com>
2023-04-10 16:41:58 -0300

    Listando jogos com flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Jogo():
@@ -7,23 +7,38 @@ class Jogo():
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Ataria')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 # Iniciar o variavel do projeto
 app = Flask(__name__)
 
+
 # Criar um nova rota
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Ataria')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 # Rodar o projeto
-app.run()
+app.run(debug=True)
 
 # trecho da app
 #app.run(host='0.0.0.0', port=8080)
